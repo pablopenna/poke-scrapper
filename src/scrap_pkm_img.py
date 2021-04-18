2386a5b3e51d4a9947cbe48b857ad2d9e6b9f3cd
Airo <pablo_penna7@hotmail.com>
2021-04-18 17:33:05 +0200

    Store data in DynamoDB table
    Known bug: back sprite for 1st gen does not follow the standard pattern (has {title}{generation} instead of {generation}{title})

--- src/scrap_pkm_img.py
+++ src/scrap_pkm_img.py
@@ -1,7 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
 import re
-import base64
+import base64 # convert img binary data to b64
+import time # get timestamp
+from decimal import Decimal # convert timestamp to decimal
 
 import argparse # lib for parsing command parameters
 import os # to get file path and be able to store images always in the img/ folder
@@ -10,21 +12,52 @@ import boto3
 
 #SPRITE_NAME_PATTERN='Spr {isBacksprite} {generation}{title} {pokenumber}{modifiers} {isShiny}.png'
 #SPRITE_NAME_REGEXP='Spr( b)?( [0-9]+)([a-z]+)( [0-9]+)([A-Z]+)?( s)?.png'
-SPRITE_URL_PATTERN=re.compile('.*Spr_{optional_isBacksprite}_{generation}{title}_{dexNumber}{optional_modifiers}_{optional_gender}_{optional_isShiny}.png')
+SPRITE_URL_PREFIX='//cdn.bulbagarden.net/upload/9/9d/'
+SPRITE_URL_PATTERN='Spr{isBacksprite}_{generation}{title}_{dexNumber}{modifiers}{gender}{isShiny}.png'
 SPRITE_URL_REGEXP=re.compile('.*Spr(_b)?(_[0-9]+)([a-z]+)(_[0-9]+)([A-Z]+)?(_[mf])?(_s)?.png')
 
-def aws_setup():
-    #session = boto3.Session(profile_name='dynamoDBFullAccess')
-    #db_client = session.client('dynamodb')
-    boto3.setup_default_session(profile_name='dynamoDBFullAccess')
+###
+# AWS TABLE SETUP AND METHODS
+###
+
+boto3.setup_default_session(profile_name='dynamoDBFullAccess')
+
+def setupAwsTable():
     dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('PokeSprites')
+    return table
 
-def getSpriteSrc(dexNumber, generation, title, isBacksprite, isShiny, modifiers):
-    _isBacksprite = "b" if isBacksprite else ""
-    _isShiny = "s" if isShiny else ""
-    #_isShiny = "_s" if isShiny else ""
+def writeSpriteToDB(spriteData):
+    table = setupAwsTable()
+    table.put_item(Item=spriteData)
 
-    return SPRITE_URL_PATTERN.format(modifiers= modifiers, isShiny=_isShiny, isBacksprite=_isBacksprite, generation=generation, title=title, pokenumber=dexNumber)
+def getSpriteFromDB(spriteData):
+    table = setupAwsTable()
+    response = table.get_item(
+        Key={
+            'name': spriteData["name"],
+            'index': spriteData["index"]
+        }
+    )
+    item = response['Item']
+    return item
+
+def writeSpriteBatchToDB(spriteDataArray):
+    table = setupAwsTable()
+    with table.batch_writer() as batch:
+        for i in range(len(spriteDataArray)):
+            batch.put_item(Item=spriteDataArray[i])
+
+###
+# POKE SPRITES SCRAPPING
+###
+
+def getSpriteSrc(isBackSprite, generation, title, dexNumber, modifiers, gender, isShiny):
+    sBacksprite = "_b" if isBackSprite else ""
+    sGender = "_"+gender if gender else ""
+    sShiny = "_s" if isShiny else ""
+
+    return SPRITE_URL_PATTERN.format(isBacksprite=sBacksprite, generation=generation, title=title, dexNumber=dexNumber, modifiers= modifiers, gender=sGender, isShiny=sShiny)
 
 def getPokemonSprites(pokeName="Bulbasaur"):
     pokeName = pokeName.lower().capitalize()
@@ -44,21 +77,26 @@ def getPokemonSprites(pokeName="Bulbasaur"):
             print("No sprites found!")
             return
 
-        for idx, image in enumerate(images_set2):
-            print(str(idx) + "\t| " +image["alt"])
-            img = requests.get("https:"+image["src"])
-            #print(img.content)
+        spriteDataArray = []
+
+        for idx, imageTag in enumerate(images_set2):
+            print(str(idx) + "\t| " +imageTag["alt"])
+            imageRequest = requests.get("https:"+imageTag["src"])
+            # print(img.content)
 
             #sprite params
-            spriteFullData = parseSpriteSrc(image["src"])
-            spriteFullData["name"] = pokeName
-            spriteFullData["sprite"] = base64.b64encode(img.content) # prepend "data:image/png;base64,"
-
+            spriteFullData = getSpriteFullData(pokeName, idx, imageTag["src"], imageRequest.content)
             print("sprite params: {}".format(spriteFullData))
 
-            #local testing
-            fileName = str(idx)+"__"+image["alt"]
-            saveImgsInFiles(fileName, img.content, True)
+            # add to total
+            spriteDataArray.append(spriteFullData)
+
+            # local testing - save images locally
+            fileName = str(idx)+"__"+imageTag["alt"]
+            saveImgsInFiles(fileName, imageRequest.content, True)
+
+        # print("This is the sprite data: {}".format(spriteDataArray))
+        return spriteDataArray
 
 def saveImgsInFiles(fileName, imgBinaryData, bOverwrite=False):
     filePath = os.path.dirname(__file__) + "/../img/"
@@ -92,10 +130,22 @@ def parseSpriteSrc(spriteImgSrc):
     }
     return sprite_params
 
+def getSpriteFullData(pokeName, index, imageSrc, imageBinaryData):
+    spriteFullData = parseSpriteSrc(imageSrc)
+    spriteFullData["name"] = pokeName # partition key
+    spriteFullData["index"] = index # used a sort key, used to create a unique id since name (partition key) is not unique
+    spriteFullData["sprite"] = base64.b64encode(imageBinaryData) # prepend "data:image/png;base64,"
+    spriteFullData["timestamp"] = int(time.time()) # DynamoDB does not accept floats
+    return spriteFullData
+
+###
+# MAIN
+###
 
 if __name__ == "__main__":
-    aws_setup()
     parser = argparse.ArgumentParser("scrap-poke-image")
     parser.add_argument("pokemon_name", help="Name of the Pokémon to get its sprites", type=str)
     args = parser.parse_args()
-    getPokemonSprites(args.pokemon_name)
+    spriteData = getPokemonSprites(args.pokemon_name)
+    # print("sprite data: {}".format(spriteData))
+    writeSpriteBatchToDB(spriteData)
